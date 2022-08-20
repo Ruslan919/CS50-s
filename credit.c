3b5897ec23a2541fc34fc257521b002b20f388d4
Ruslan H <ruslanh@example.com>
2022-08-20 21:22:53 +0300

    “Second”

---
+++ credit.c
@@ -0,0 +1,20 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+
+// Promp for input.
+long input = 4003600000000014;
+
+// Calculate checksum.
+
+for {
+  
+}
+
+// Check for card length and staring digits.
+
+// Print AMEX, MASTERCARD, VISA or INVALID.
+
+}
